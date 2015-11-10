5a0b3f6cc9b55a87797c1d4092695c948a436fa9
Kyle Pontius <kbpontius@gmail.com>
2015-11-10 14:34:08 -0700

    Initial Commit

---
+++ Lab 1 - Hot Plate (C)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab 1 - Hot Plate (C)
+//
+//  Created by Kyle on 11/10/15.
+//  Copyright © 2015 Kyle Pontius. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
